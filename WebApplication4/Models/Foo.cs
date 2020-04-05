54c43617280d5d9b8f82920669c355e79bf726b2
Katsuhiro Nishida <ipgwarts@outlook.jp>
2020-04-05 14:27:26 +0900

    a

---
+++ WebApplication4/Models/Foo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public enum Abc { A, B, C }
+    public class Foo
+    {
+        [UIHint("Abc")]
+        public Abc MyProperty { get; set; }
+    }
+}
